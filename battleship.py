999ecb99bb8aefb8408c03e8dfa31bd02510058b
Sunitha101199 <sunitha101199@msitprogram.net>
2021-09-30 12:25:58 +0530

    updated

--- battleship.py
+++ battleship.py
@@ -322,5 +322,5 @@ def runSimulation(w, h):
 # This code runs the test cases to check your work
 if __name__ == "__main__":
     ## Finally, run the simulation to test it manually ##
-    # runSimulation(500, 500)
-    test.testGetClickedCell()
+    runSimulation(500, 500)
+    # test.testGetClickedCell()
